3308e76f0d78f566729678a54dcf5217700c435a
Yago Souza <yagos-s@live.com>
2024-09-13 17:37:34 -0300

    Criando métodos save, findAll, findByCep, findById, update e delete na api.

--- src/main/java/br/com/fiap/parking_meter/parking_meter/repository/ParkingSpotRepository.java
+++ src/main/java/br/com/fiap/parking_meter/parking_meter/repository/ParkingSpotRepository.java
@@ -2,8 +2,16 @@ package br.com.fiap.parking_meter.parking_meter.repository;
 
 import br.com.fiap.parking_meter.parking_meter.model.ParkingSpot;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+import java.util.Optional;
+
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpot, String> {
+
+    // Método para buscar todos os ParkingSpots por CEP
+    List<ParkingSpot> findByCep(String cep);
+
+    Optional<ParkingSpot> findByCepAndLocation(String cep, String location);
+
+    void deleteByCepAndLocation(String cep, String location);
 }
